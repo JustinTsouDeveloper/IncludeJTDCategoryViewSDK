cd0d44bcce89c004e5fb6ce843d4e5e9b7e4e77f
Justin Tsou <killkill8250@gmail.com>
2021-02-23 17:31:25 +0800

    Add static JTDCategoryView framework in project.

--- IncludeJTDCategoryViewSDK/ViewController.swift
+++ IncludeJTDCategoryViewSDK/ViewController.swift
@@ -6,12 +6,16 @@
 //
 
 import UIKit
+import JTDCategoryView
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let categoryView = JTDCategoryView.init(categorys: ["Test 0","Test 1"], frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.size.width, height: 100))
+        categoryView.center = self.view.center
+        self.view.addSubview(categoryView)
     }
 
 
